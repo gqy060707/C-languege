7aef60b4ace07d2f79b31cc97c59e98ffad97fcc
gqy060707 <3310769820@qq.com>
2026-04-18 00:14:49 +0800

    second

---
+++ 26.4.14.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define InitSize 10
+typedef struct {
+    int *data;
+    int length;
+    int MaxSize;
+
+}SeqList;
+
+void InitList( SeqList *L){//创建顺序表
+	L->data = (int *)malloc(InitSize*sizeof(int));//分配内存空间
+	L->length = 0;//初始长度为0
+	L->MaxSize = InitSize;//初始最大长度为InitSize 10
+} 
+
+void IncreaseSize(SeqList *L,int len){//增加顺序表的长度
+	int *p = L->data;//保存原来顺序表的地址
+	L->data=(int *)malloc((L->MaxSize+len)*sizeof(int));//重新分配内存空间
+	for(int i=0 ; i< L->length; i++){//将原来顺序表中的元素复制到新的顺序表中
+		L->data[i]=p[i];
+	}
+	L->MaxSize=L->MaxSize+len;//更新顺序表的最大长度
+	free(p);//释放原来顺序表的内存空间
+}
+
+void ListInsert(SeqList *L,int i,int e){
+	for(int j=L->length;j>=i;j--){
+		
+	}
+	
+}
+int main(){
+	SeqList L;
+	InitList(&L);
+	IncreaseSize(&L,5);
+	return 0;
+}
---
+++ kk.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define InitSize 10
+typedef struct{
+    int *data;
+    int MaxSize;
+    int length;
+}SequenceList;
+
+void InitialList(SequenceList *L){
+    L->data=(int*)malloc(InitSize*sizeof(int));
+    if(L->data == NULL){
+        printf("内存分配失败！\n");
+        exit(1);
+    }
+    L->MaxSize=InitSize;
+    L-> length=0;
+}
+
+void IncreaseList(SequenceList *L,int length){
+    int *p = L->data;
+    L->data=(int *)malloc((L->MaxSize+length)*sizeof(int));
+    if(L->data == NULL){
+        printf("内存分配失败！\n");
+        exit(1);
+    }
+    for(int i=0;i<L->length;i++){                                                                                                                                                                                             
+        L->data[i]=p[i];
+    }
+    L->MaxSize+=length;
+    free(p);
+}
+int main(){
+    SequenceList L;
+    InitialList(&L);
+    IncreaseList(&L,5);
+    return 0;
+}
